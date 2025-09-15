1f75e8ef31853dc67c3f60412c7ee36515173e99
janelleg22 <janelleg22@nycstudents.net>
2025-09-15 16:09:48 +0000

    did tasks

---
+++ Java-3-Activity/Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    System.out.println("Janelle Gonzalez");
+    System.out.println("Janelle Gonzale, 123  java ave");
+    System.out.println("Janelle Gonzalez\n 123  java ave");
+
+  }
+
+  
+}
