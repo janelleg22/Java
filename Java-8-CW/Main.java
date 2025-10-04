4e19afb5801756fc026ae07bef20dfb08d917239
janelleg22 <janelleg22@nycstudents.net>
2025-10-04 17:26:34 +0000

    did tasks

--- Java-8-CW/Main.java
+++ Java-8-CW/Main.java
@@ -2,26 +2,29 @@ class Main {
   public static void main(String[] args) {
     (new Main()).init();
   }
+
   // define the functions
-
-  void init(){
-    // call the functions
-  
   void madlib(String noun, String adj, String verb){
-    System.out.println("The "+ adj + " " + noun+" " + verb);
+    System.out.println("The "+ adj + " " + noun + " " + verb);
   }
 
-  double areaOfSquare(double s){
-    return s*s;
+  double areaSq(double s){
+    return s * s;
   }
-  
+   
   double areaOfCircle(double radius){ 
     return Math.PI*Math.pow(radius,2); 
   }
-  // double result = areaSq(2.5);
-  // System.out.println("Area is" + result);
-  }
 
- 
-  
+
+  void init() {
+    madlib("person", "loud", "runs");
+
+    double result = areaSq(2.5);
+    System.out.println("Area is " + result);
+
+     result = areaOfCircle(3);
+    System.out.println("Area of a circle is " + result);
+
+  }
 }
