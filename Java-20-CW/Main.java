f6681fc3e92267e344d0441d4f79269c9bd2911d
janelleg22 <janelleg22@nycstudents.net>
2025-11-14 17:29:42 +0000

    did tasks

---
+++ Java-20-CW/Main.java
@@ -0,0 +1,42 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init(){
+
+   
+  }
+  /* Problem 1: Eggs
+    After a farmer disastrously breaks and loses count of her eggs, she experiences difficulty recalling her original quantity. When questioned about the incident, she is able to remember that her eggs could not be evenly divided into twos, threes, fours, fives, or sixes without having a single egg left over each time. The only number by which the quantity could be evenly divided into was seven. How many eggs did she have originally?
+    Write a function eggs() that returns the quantity of eggs she originally had.
+  */
+  int eggs(){
+    int numberOfEggs = 7;
+      while ( !(numberOfEggs % 2 == 1 && 
+      numberOfEggs % 3 == 1 && 
+      numberOfEggs % 4 == 1 && 
+      numberOfEggs % 5 == 1 &&
+      numberOfEggs % 6 == 1) ) {
+        numberOfEggs+=7;
+      }
+      return numberOfEggs;
+  }
+
+
+    /* Problem 2:
+  Write a function intersect() that returns the value of x where two linear equations intersect.  Equations are:  y=x+3  and  y=12-2x.
+  (Given: x>0 and x is a whole number)
+    */
+double intersect(){
+  int x = 0;
+  while (x+3 1 !- 12-1*x){
+    x++;
+  }
+  return x;
+}
+
+  
+}
