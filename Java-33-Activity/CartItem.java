e97ada8841f05b22a55c1748c4e42dd5244676be
janelleg22 <janelleg22@nycstudents.net>
2026-01-23 16:53:08 +0000

    did tasks

---
+++ Java-33-Activity/CartItem.java
@@ -0,0 +1,29 @@
+class CartItem{
+  // variables
+  String itemName;
+  double itemPrice;
+  int quantity;
+  boolean onSale;
+
+  //1: Constructor
+  CartItem(String itemName, double itemPrice, int quantity, boolean onSale){
+    this.itemName = itemName;
+    this.itemPrice = itemPrice;
+    this.quantity = quantity;
+    this.onSale = onSale;
+  }
+  
+  
+  //2: fn getPrice() to get discounted price (if applicable)
+  double getPrice(){
+    if(this.onSale){
+      return this.itemPrice * .9;
+    }
+    else{
+      return this.itemPrice;
+    }
+  }
+
+  
+
+}
---
+++ Java-33-Activity/Main.java
@@ -0,0 +1,36 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  
+  void print(Object o){System.out.println(o);}
+  void printt(Object o){System.out.print(o);}
+
+  void init(){
+
+	//3: create 3 new item objects
+    CartItem item1 = new CartItem("Tomato", 1.99, 3, true);
+    CartItem item2 = new CartItem("Apple", 2.99, 3, false);
+    CartItem item3 = new CartItem("Avacado", 1.79, 3, true);
+
+
+
+	
+	//4: display name & original price of items that are on sale
+  if(item1.onSale){
+    print("Name" +item1.itemName);
+    print("Price" + item1.itemPrice);
+  }
+  if(item3.onSale){
+    print("Name" +item3.itemName);
+    print("Price" + item3.itemPrice);
+  }
+	
+	//5: subtotal (for all quantities of all items in cart, using discounted prices)
+  double subtotal = item1.getPrice()	
+	// display subtotal, tax and total
+    
+  }
+
+}
